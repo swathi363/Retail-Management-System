e81e994e16cb22ca8853a87e1f92173cdbb3d0b1
G3-K <gayu.kunnath@gmail.com>
2020-09-15 15:50:37 +0530

    deleted class fasil

--- Retail Management System/Retail Management System/Models/User.cs
+++ Retail Management System/Retail Management System/Models/User.cs
@@ -14,10 +14,10 @@ namespace Retail_Management_System.Models
         [DataType(DataType.EmailAddress)]
         public string UserId { get; set; }
         [Required]
-        [Display(Name = "Firstname")]
+        [Display(Name = "First name")]
         public string Firstname { get; set; }
         [Required]
-        [Display(Name = "Lastname")]
+        [Display(Name = "Last name")]
         public string Lastname { get; set; }
         [Required(ErrorMessage = "Password is required")]
         [Display(Name = "Password")]
